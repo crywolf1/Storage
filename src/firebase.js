45736d60973232050cf6f13886e1c3f9052d6fed
Dan <89650681+crywolf1@users.noreply.github.com>
2022-06-15 16:55:56 +0430

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,6 +4,26 @@ import { getStorage } from "firebase/storage";
 import { GoogleAuthProvider } from "firebase/auth";
 import { getAuth } from "firebase/auth";
 
+
+//copy and paste your own firebase config here!
+
+
+/*const firebaseConfig = {
+  apiKey: "************kGlvds2o",
+
+  authDomain: "storage-fireb**********firebaseapp.com",
+
+  projectId: "storage-firebas*******",
+
+  storageBucket: "storage-firebase*******.com",
+
+  messagingSenderId: "****16**9",
+
+  appId: "1:640390************475cc",
+}; */ 
+
+
+
 import "firebaseui/dist/firebaseui.css";
 
 
